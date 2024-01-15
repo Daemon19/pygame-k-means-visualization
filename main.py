6dc835648dda433ddee3766e308e9b54321fa242
Daemon19 <komanglangendria04@gmail.com>
2024-01-16 01:04:28 +0800

    Draw lines from data to the closest centroids

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
     data = [pos * [SCREEN_W, SCREEN_H] for pos in np.random.rand(100, 2)]
     centroids = [pos * [SCREEN_W, SCREEN_H] for pos in np.random.rand(3, 2)]
 
+    centroids_data = [[] for _ in centroids]
+    for d in data:
+        centroids_data[closest_to(d, centroids)].append(d)
+    
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -23,8 +27,12 @@ def main():
 
         screen.fill("black")
 
+        for cd, c in zip(centroids_data, centroids):
+            for p in cd:
+                pg.draw.line(screen, "white", p, c)
+
         for x, y in data:
-            pg.draw.circle(screen, "white", (x, y), 2)
+            pg.draw.circle(screen, "red", (x, y), 2)
 
         for x, y in centroids:
             pg.draw.circle(screen, "green", (x, y), 3)
@@ -34,6 +42,10 @@ def main():
 
         pg.display.update()
         clock.tick(60)
+
+
+def closest_to(point, centroids):
+    return min(enumerate(map(lambda c: pg.Vector2(c[0], c[1]).distance_squared_to(point), centroids)), key=lambda l: l[1])[0]
 
 
 if __name__ == "__main__":
